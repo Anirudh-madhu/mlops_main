8120965853ef65d2b7c12367bfe6205cf84ef756
Anirudh Madhusudan <anirudhmadhusudan854@gmail.com>
2021-05-12 12:10:34 +0530

    config read

--- src/pipline_01_data_preparation.py
+++ src/pipline_01_data_preparation.py
@@ -1,16 +1,29 @@
 import os
 import argparse
-import _yaml
+import yaml
 import logging
+
+
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config= yaml.safe_load(yaml_file)
+    return config
+
+
+def main(config_path, data_source):
+    config= read_params(config_path)
+    print(config)
 
 
 
 
 if __name__=="__main__":
     ags= argparse.ArgumentParser()
-    ags.add_argument("..config",default= "default")
-    ags.add_argument("..data source",default=None)
+    default_config_file= os.path.join("config", "params.yaml")
+    ags.add_argument("--config",default= default_config_file)
+    ags.add_argument("--datasource",default=None)
 
     parsed_args= ags.parse_args()
+    main(config_path= parsed_args.config, data_source= parsed_args.datasource)
 
 
